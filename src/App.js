3dff44499d2327bc1c0a5dc9d09c29794834ffbb
mawarutuco <xul4841268@gmail.com>
2022-10-22 22:17:29 +0800

    把component塞進app、Provider包住所有元件

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store/index';
+import TodoList from './component/TodoList';
+import TodoAdd from './component/TodoAdd';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <TodoAdd />
+      <TodoList />
+    </Provider>
   );
 }
-
-export default App;
